f0cd5f85cee4b4aaea32e3877dd90f2aa01d0bcc
esmattt77 <esmatt88@gmail.com>
2025-09-22 07:15:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 
 # Import API clients
 from viotp_api import VIOTPAPI
-from smsman_api import SmsManApi  # استيراد الفئة الصحيحة
+from smsman_api import get_smsman_balance, get_smsman_countries, request_smsman_number, get_smsman_code, cancel_smsman_request
 from tiger_sms_api import TigerSMSAPI
 
 # Import handlers
@@ -32,7 +32,9 @@ TIGER_SMS_API_KEY = os.environ.get('TIGER_SMS_API_KEY')
 # Create API client objects
 viotp_client = VIOTPAPI(VIOTP_API_KEY)
 tiger_sms_client = TigerSMSAPI(TIGER_SMS_API_KEY)
-smsman_api = SmsManApi(SMSMAN_API_KEY) # إنشاء كائن من الفئة
+smsman_api = {'get_smsman_balance': get_smsman_balance, 'get_smsman_countries': get_smsman_countries,
+              'request_smsman_number': request_smsman_number, 'get_smsman_code': get_smsman_code,
+              'cancel_smsman_request': cancel_smsman_request}
 
 # Create a Flask app instance
 app = Flask(__name__)
@@ -54,3 +56,4 @@ def webhook():
 if __name__ == '__main__':
     bot.set_webhook(url=WEBHOOK_URL + TELEGRAM_BOT_TOKEN, allowed_updates=['message', 'callback_query'])
     app.run(host='0.0.0.0', port=PORT)
+
